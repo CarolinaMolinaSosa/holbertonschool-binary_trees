7c317879dd5f27eabb0026f831f58f50fb8e6354
root <Molinasosacarolina17@gmail.com>
2023-05-11 05:45:33 -0700

    Fix Betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,10 +2,10 @@
 
 /**
  * binary_tree_node - function that creates a new node
- * 
+ *
  * @parent: parent of the nodes
  * @value: value of the nodes
- * 
+ *
  * Return: new node
 */
 
@@ -14,13 +14,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
     if (new_node == NULL)
-        return(NULL);
-    
+        return (NULL);
+
     new_node->parent = parent;
     new_node->n = value;
     new_node->left = NULL;
     new_node->right = NULL;
 
-    return(new_node);
+    return (new_node);
 
 }
