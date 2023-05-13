198adfb586809cd06778585c9732f81dc97a5b7f
Faqu22 <facundoalvarez415@gmail.com>
2023-05-13 13:41:19 -0300

    Finally the Betty's problems

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,11 +9,11 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
-    if (tree->left)
-        binary_tree_postorder(tree->left, func);
-    if (tree->right)
-         binary_tree_postorder(tree->right, func);
-    func(tree->n);    
+	if (!tree || !func)
+		return;
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int i = 0;
-    int n = 0;
+	int i = 0;
+	int n = 0;
 
-    if (!tree)
-        return (0);
-    if (tree->left)
-        i = 1 + binary_tree_height(tree->left);
-    if (tree->right)
-        n = 1 + binary_tree_height(tree->right);
-    return (i < n) ? n : i;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		i = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		n = 1 + binary_tree_height(tree->right);
+	return ((i < n) ? n : i);
 }
