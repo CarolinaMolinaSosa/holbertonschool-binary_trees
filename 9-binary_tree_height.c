ce70f68061b7298176191767857f7a97ded1a64a
Faqu22 <facundoalvarez415@gmail.com>
2023-05-13 12:55:49 -0300

    Cont

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    int i = 0;
+    int n = 0;
+
+    if (tree->left)
+        i = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        n = 1 + binary_tree_height(tree->right);
+    return (i < n) ? n : i;
+}
